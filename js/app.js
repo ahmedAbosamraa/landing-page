7254e7ef06219c0fd00e925988ffb598800ea598
ahmedAbosamra <ahmed__abosamra@hotmail.com>
2022-01-24 14:16:03 +0200

    edit js

--- js/app.js
+++ js/app.js
@@ -139,49 +139,40 @@ const observer = new IntersectionObserver(observerCallback, observerOptions);
 
 sections.forEach((sec) => observer.observe(sec));
 */
-// Cache selectors
-
-var lastId,
-    topMenu = document.getElementById("#navbar__list"),
-    topMenuHeight = (topMenu.outer() + 1),
-    // All list items
-    menuItems = topMenu.find("a"),
-    // Anchors corresponding to menu items
-    scrollItems = menuItems.map(function () {
-        var item = $($(this).attr("href"));
-        if (item.length) { return item; }
-    });
-
-// Bind click handler to menu items
-// so we can get a fancy scroll animation
-menuItems.click(function (e) {
-    var href = $(this).attr("href"),
-        offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
-    $('html, body').stop().animate({
-        scrollTop: offsetTop
-    }, 850);
-    e.preventDefault();
-});
-
-// Bind to scroll
-$(window).scroll(function () {
-    // Get container scroll position
-    var fromTop = $(this).scrollTop() + topMenuHeight;
-
-    // Get id of current scroll item
-    var cur = scrollItems.map(function () {
-        if ($(this).offset().top < fromTop)
-            return this;
-    });
-    // Get the id of the current element
-    cur = cur[cur.length - 1];
-    var id = cur && cur.length ? cur[0].id : "";
-
-    if (lastId !== id) {
-        lastId = id;
-        // Set/remove active class
-        menuItems
-            .parent().removeClass("active")
-            .end().filter("[href=#" + id + "]").parent().addClass("active");
+console.log(allSections);
+function active(section) {
+    const activeElement = section.getBoundingClientRect();
+    return activeElement.top > 0 && activeElement.top <= 300
+} function makeActive() {
+    for (const section of allSections) {
+        console.log(section);
+        let links = document.querySelectorAll(`a[href="#${section.id}"]`);
+        console.log(links);
+        if (active(section)) {
+            section.classList.add("your-active-class");
+            //link.classList.add("active");
+        } else {
+            section.classList.remove("your-active-class");
+            // link.classList.remove("active");
+        }
     }
-});
+}
+window.addEventListener("scroll", makeActive);
+        /*
+function toggleActiveClass() {
+allSections.forEach(section => {
+const link = document.querySelector(`a[href="#${section.id}"]`);
+const sectionTop = section.getBoundingClientRect().top;
+if (sectionTop >= 0 && sectionTop <= 300) {
+section.classList.add("active");
+link.classList.add('your-active-class');
+}
+else {
+section.classList.remove("your-active-class");
+link.classList.remove('active');
+
+}
+})
+}
+window.addEventListener("scroll", toggleActiveClass);
+*/
